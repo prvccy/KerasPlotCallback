66a1cecfd518d928d780e6ff076e96c98722bb58
prvccy <258548495@qq.com>
2018-08-08 19:14:51 +0800

    Update plotcallback.py

--- plotcallback.py
+++ plotcallback.py
@@ -2,110 +2,227 @@
 # All rights reserved. MIT License.
 
 
-from IPython.display import clear_output
 from keras.callbacks import Callback
+import pandas as pd
 import matplotlib.pyplot as plt
-from matplotlib.gridspec import GridSpec
+from IPython.display import clear_output
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import recall_score,precision_score,f1_score,roc_auc_score
 import seaborn as sns
 sns.set(style='whitegrid', palette="muted", color_codes=True) 
-import numpy as np
-import pandas as pd
-from sklearn.metrics import recall_score,precision_score,f1_score,roc_auc_score
-from sklearn.metrics import confusion_matrix
-import math
-from keras.utils.np_utils import to_categorical
+
+class PrvKCbkDataGen(object):
+    '''
+    generate data on epoch end
+    self.data: dict
+    level1 key: class, each class corresponding to one subplot
+    level2 key: attribute
+    level2 key: "plotclass" data will be plot using "plotclass"
+    '''
+    data = dict()
+    
+    def on_epoch_end(self, epoch, logs, predicts):
+        raise NotImplementedError("on_epoch_end")
+    
+    def getData(self):
+        return self.data
+
+class PrvChart(object):
+    '''
+    plot data using matplotlib
+    '''
+    def plot(self):
+        raise NotImplementedError("on_epoch_end")
 
 
-class ClassificationPlotCallback(Callback):
-    def __init__(self, evas=['auc','f1','confusion_matrix']):
-        self.cell_size = (6, 4)
-        self.max_cols = 2
-        self.percentile = percentile
-        self.metrics = []
-        self.aucs = [] if 'auc' in evas else None
-        self.f1_score = [] if 'f1' in evas else None
-        self.recall = [] if 'f1' in evas else None
-        self.precision = [] if 'f1' in evas else None
-        self.confusion_matrix = 'confusion_matrix' in evas
+class PrvKCbkController(object):
+    '''
+    use generated data to do sth useful, like earlystop savebestmodel
+    '''
+    def on_epoch_end(self, data={}):
+        raise NotImplementedError("on_epoch_end")
+        
+    def on_training_end(self, data={}):
+        pass
+
+class PrvKerasCbk(Callback):
+    '''
+    callback add to callbacks list of keras.model.fit 
+    '''
+    def __init__(self, savefile, path, dataset=None, datagens=[], controllers=[], plotcols = 2, subplotsize = (6,4)):
+        self.datagens = datagens
+        loss = PCDGLossAndMetrics()
+        self.datagens.insert(0,loss)
+        self.controllers = controllers
+        self.plotcols = plotcols
+        self.subplotsize = subplotsize
+        self.dataset = dataset
+        self.data = dict()
+        self.savefile = savefile
+        self.path = path
 
 
     def on_epoch_end(self, epoch, logs={}):
-        self.metrics.append(logs.copy())
         clear_output(wait=True)
-        plots = []
-        for k in self.metrics[0]:
-            if k.startswith('val_'):
-                continue
-            plots.append(k)
+        predicts = dict()
+        # TODO handle no testset error
+        predicts['test_y'] = self.validation_data[1]
+        predicts['test_pred'] = self.model.predict(self.validation_data[0])
+        predicts['test_price'] = self.dataset['price']
+        
+        if self.dataset is not None and 'y_validation' in self.dataset:
+            predicts['cv_y'] = self.dataset["y_validation"]
+            predicts['cv_pred'] = self.model.predict(self.dataset["X_validation"])
+            predicts['cv_price'] = self.dataset['price_validation']
+            
+        # first run data generator
+        for dg in self.datagens:
+            dg.on_epoch_end(epoch, logs, predicts)
+            self.data.update(dg.getData())
 
-        y_pred = self.model.predict(self.validation_data[0])
-        if self.y_pred.shape[1]>1:
-            y_pred = to_categorical(self.y_pred.argmax(axis=1))
-        else:
-            y_pred = self.y_pred.round(0)
+        # then plot
+        numofsubplot = 0
+        for p in self.data:
+            if 'plotclass' in self.data[p]:
+                numofsubplot += 1
+
+        self.rows = numofsubplot//self.plotcols + int(numofsubplot%self.plotcols>0)
+        fig, axes = plt.subplots(nrows=self.rows, ncols=self.plotcols, figsize=(self.subplotsize[0] * self.plotcols, self.subplotsize[1] * self.rows)) #, sharex=True 
+        axs = axes.flatten()
         
-        if self.aucs is not None:
-            self.aucs.append(roc_auc_score(self.validation_data[1], y_pred))
-            plots.append('auc')
-        
-        if self.f1_score is not None:
-            self.f1_score.append(f1_score(self.validation_data[1], y_pred))
-            self.recall.append(recall_score(self.validation_data[1], y_pred))
-            self.precision.append(precision_score(self.validation_data[1], y_pred))
-            plots.append('f1-score')
-            
-        if self.confusion_matrix:
-            if y_pred.shape[1]>1:
-                cm = confusion_matrix(self.validation_data[1].argmax(axis=1), y_pred.argmax(axis=1))
-            else:
-                cm = confusion_matrix(self.validation_data[1], y_pred.round(0))
-            plots.append('confusion_matrix')
-            
-        cols = self.max_cols
-        if 'confusion_matrix' in plots and y_pred.shape[1] < 9:
-            rows = math.ceil( len(plots) / self.max_cols)
-        else:
-            plotd = plots.copy()
-            plotd.remove('confusion_matrix')
-            rows = math.ceil( len(plotd) / self.max_cols) + 2
-            
-        fig = plt.figure(figsize=(self.cell_size[0] * cols, self.cell_size[1] * rows))
-        gs = GridSpec(rows, cols)
+        plt.xticks(range(1, self.params['epochs'] + 1))
+        plt.xlim(1, self.params['epochs'])
+        plt.locator_params(axis='x', nbins=10)
         
         subplot_No = 0
-        for p in plots:
-            row = math.floor(subplot_No/self.max_cols)
-            col = subplot_No - row * self.max_cols
-            subplot_No += 1
-            
-            if p == 'confusion_matrix' and y_pred.shape[1] > 8:
-                plt.subplot(gs[-2:, :])
-            else:
-                plt.subplot(gs[row, col])
-            
-            
-            if p == 'confusion_matrix':
-                sns.heatmap(pd.DataFrame(cm, range(len(cm)), range(len(cm)) ), annot=True, cmap="YlGnBu") 
-                continue
-            
-            plt.xticks(range(1, self.params['epochs'] + 1))
-            plt.xlim(1, self.params['epochs'])
-            plt.locator_params(axis='x', nbins=10)
-            
-            # plot loss and metrics in model
-            if p in self.metrics[0]:
-                plt.plot(range(1, len(self.metrics) + 1), [e[p] for e in self.metrics], label=p)
-                if 'val_'+p in self.metrics[0]:
-                    plt.plot(range(1, len(self.metrics) + 1), [e['val_'+p] for e in self.metrics], label='val_'+p)
-                    
-            if p == 'auc':
-                plt.plot(range(1, len(self.aucs) + 1), self.aucs, label="auc")
+        for p in self.data:
+            if 'plotclass' in self.data[p]:
+                subplot = eval(self.data[p]['plotclass']+'()')
+                subplot.plot(axs[subplot_No], self.data[p], p)
+                subplot_No += 1
                 
-            if p == 'f1-score':
-                plt.plot(range(1, len(self.f1_score) + 1), self.f1_score, label="f1-score")
-                plt.plot(range(1, len(self.recall) + 1), self.recall, label="recall")
-                plt.plot(range(1, len(self.precision) + 1), self.precision, label="precision")
-                
-            plt.legend(loc='center right')
         plt.tight_layout()
         plt.show();
+        # last run controller
+        for c in self.controllers:
+            c.on_epoch_end(self.data)
+            if epoch == self.params['epochs']:
+                c.on_training_end(self.data)
+
+
+
+
+
+                
+class PrvLineChart(PrvChart):
+    def plot(self, ax, plotdata, p):
+        for key in plotdata:
+            if key != 'plotclass':
+                ax.plot(range(1, len(plotdata[key])+1), plotdata[key], label=key)
+                ax.set_title(p,fontsize=12,color='r')
+        ax.legend()
+
+class PCDGLossAndMetrics(PrvKCbkDataGen):
+    metrics = []
+    def on_epoch_end(self, epoch, logs, predicts):
+        self.metrics.append(logs.copy())
+        for k in self.metrics[0]:
+            if not k.startswith('val_'):
+                if k not in self.data:
+                    self.data.update({k:{}})
+                self.data[k][k] = [epoch[k] for epoch in self.metrics]
+                self.data[k]['val_'+k] = [epoch['val_'+k] for epoch in self.metrics]
+                self.data[k]['plotclass'] = 'PrvLineChart'
+
+
+class PCDGConfusionMatrix(PrvKCbkDataGen):
+    def on_epoch_end(self, epoch, logs, predicts):
+        for dataset in ['test','cv']:
+            if not dataset+'_y' in predicts:
+                continue
+            self.data.update({'ConfusionMatrix_' + dataset:{}})
+            if predicts[dataset + '_pred'].shape[1]>1:
+                cm = confusion_matrix(predicts[dataset + '_y'].argmax(axis=1), predicts[dataset + '_pred'].argmax(axis=1))
+            else:
+                cm = confusion_matrix(predicts[dataset + '_y'], predicts[dataset + '_pred'].round(0))
+            self.data['ConfusionMatrix_' + dataset]['ConfusionMatrix_' + dataset] = cm
+            self.data['ConfusionMatrix_' + dataset]['plotclass'] = 'PrvHeatMap'
+            
+      
+            
+class PrvHeatMap(PrvChart):
+    def plot(self, ax,  plotdata, p):
+        for key in plotdata:
+            if key != 'plotclass':
+                cm = plotdata[key]
+                sns.heatmap(pd.DataFrame(cm, range(len(cm)), range(len(cm)) ), annot=True, cmap="YlGnBu", ax = ax) 
+
+
+class PCDGF1Score(PrvKCbkDataGen):
+    f1_score = []
+    recall = []
+    precision = []
+    def on_epoch_end(self, epoch, logs, predicts):
+        for dataset in ['test','cv']:
+            if not dataset+'_y' in predicts:
+                continue
+            self.f1_score.append(f1_score(predicts[dataset + '_y'], predicts[dataset + '_pred'],average='samples'))
+            self.recall.append(recall_score(predicts[dataset + '_y'], predicts[dataset + '_pred'],average='samples'))
+            self.precision.append(precision_score(predicts[dataset + '_y'], predicts[dataset + '_pred'],average='samples'))
+
+            self.data.update({'F1':{}})
+            self.data['F1']['F1'] = self.f1_score
+            self.data['F1']['recall'] = self.recall
+            self.data['F1']['precision'] = self.precision
+            self.data['F1']['plotclass'] = 'PrvLineChart'
+             
+                
+class PCDGROCAUC(PrvKCbkDataGen):
+    aucs = []
+    def on_epoch_end(self, epoch, logs, predicts):
+        for dataset in ['test','cv']:
+            if not dataset+'_y' in predicts:
+                continue
+            self.aucs.append(roc_auc_score(predicts[dataset + '_y'], predicts[dataset + '_pred']))
+
+            self.data.update({'AUC':{}})
+            self.data['AUC']['AUC'] = self.aucs
+            self.data['AUC']['plotclass'] = 'PrvLineChart'
+
+
+class PKCSaveModelAndResult(PrvKCbkController):
+    def __init__(self, path):
+        now = datetime.datetime.now()
+        self.savefile = path + '/' + now.strftime('%Y%m%d-%H:%M:%S:')+str(now.microsecond)
+        
+    def on_epoch_end(self, epoch, data={}):
+        #save best model
+        if 'cv_alpha_fees' in data:
+            key = 'cv_alpha_fees'
+        else:
+            key = 'test_alpha_fees'
+            
+        if data[key][-1] == max(data[key]):
+            files = os.listdir(self.path)
+            for file in files:
+                if file.startswith(self.savefile) and file.endswith('model'):
+                    os.remove(self.path+file)
+            self.model.save(self.path + self.savefile + '_alpha' + \
+                        str(self.alpha_fees_short[-1]) + '_buyq' + \
+                        str(self.buyq_short[-1]) + '_sellq' + str(self.sellq_short[-1]) + '.model')
+        
+    def on_training_end(self, data={}):
+        #save training data
+        with h5py.File(self.path+self.savefile, 'w') as hf:
+            for key1 in self.data.keys():
+                for key2 in self.data[key1].keys():
+                    try:
+                        if self.data[key1][key2] == None:
+                            hf[key1+'/'+key2] = 0
+                        else:
+                            hf[key1+'/'+key2] = self.data[key1][key2]
+                    except TypeError:
+                        self.data[key1][key2].to_hdf(self.path+self.savefile,key1+'/'+key2)
+            
+
+                
